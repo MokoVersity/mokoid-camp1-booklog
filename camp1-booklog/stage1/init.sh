fd28ab0c5287a58a4c44abc7ec7780ca25d20f82
Hank Wang <drapho@gmail.com>
2013-09-10 04:38:27 +0800

    script to init structure

---
+++ camp1-booklog/stage1/init.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+mkdir -p public/js
+touch public/js/main.js
+mkdir -p public/css
+touch public/css/style.css
+mkdir -p public/images
+mkdir -p public/vendor
+mkdir -p public/fonts
+mkdir -p views
+mkdir -p sass
+mkdir -p schema
+mkdir -p scripts
+mkdir -p utilities
+mkdir -p fixes
+touch LICENSE
+touch Gruntfile.js
+touch package.json
+touch bower.json
+touch .bowerrc
+touch .gitignore
+touch .editorconfig
