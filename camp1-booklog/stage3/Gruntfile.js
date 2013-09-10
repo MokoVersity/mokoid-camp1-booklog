81243ca42e595cd9e58256f3f716e0b27bf89749
Hank Wang <drapho@gmail.com>
2013-09-10 21:57:48 +0800

    Added grunt-contrib-sass

--- camp1-booklog/stage3/Gruntfile.js
+++ camp1-booklog/stage3/Gruntfile.js
@@ -1,5 +1,26 @@
 module.exports = function(grunt) {
     grunt.initConfig({
+        sass: {
+            dev: {
+                options: {
+                    style: 'expanded',
+                    noCache: true
+                },
+                files: {
+                    'public/css/style.css': 'sass/style.sass',
+                    'public/css/wiki.css': 'sass/wiki.sass',
+                }
+            },
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'public/css/style.css': 'sass/style.sass',
+                    'public/css/wiki.css': 'sass/wiki.sass',
+                }
+            }
+        },
         exec: {
             start_server: {
                 cmd: 'NODE_ENV=production node app.js'
@@ -12,6 +33,7 @@ module.exports = function(grunt) {
 
     // load plugins
     grunt.loadNpmTasks('grunt-exec');
+    grunt.loadNpmTasks('grunt-contrib-sass');
 
     // default task(s)
     grunt.registerTask('start', ['exec:start_server']);
