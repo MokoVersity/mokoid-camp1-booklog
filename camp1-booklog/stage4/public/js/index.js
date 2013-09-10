ff87c91bdef97fa4f049ed7158fbe28a4519c12e
Hank Wang <drapho@gmail.com>
2013-09-10 23:44:33 +0800

    render author from Backbone

--- camp1-booklog/stage4/public/js/index.js
+++ camp1-booklog/stage4/public/js/index.js
@@ -1,15 +1,41 @@
 var app = app || {};
 
-app.MainView = Backbone.View.extend({
-    initialize: function() {
-        app.mainView = this;
+app.Author = Backbone.Model.extend({
+    defaults: {
+        "name":  "Jollen Chen",
+        "hash": "400c529007e2242ac7132c78fc772e91"
+    }
+});
 
+app.AuthorView = Backbone.View.extend({
+    el: '#author',
+    template: _.template($('#tmpl-author').html()),
+    initialize: function() {
+        this.model = new app.Author();
+        this.render();
+    },
+    render: function() {
+        console.log(this.template(this.model.attributes));
+        this.$el.html(this.template(this.model.attributes));
+    }
+});
+
+app.WikiView = Backbone.View.extend({
+    initialize: function() {
         if ($('#markdown').length > 0) {
             var converter = new Showdown.converter();
             var markdown = $('#markdown').html();
             var html = converter.makeHtml(markdown);
             $('.wiki-content').html(html);
         }
+    }
+});
+
+app.MainView = Backbone.View.extend({
+    initialize: function() {
+        app.mainView = this;
+        app.wikiView = new app.WikiView();
+        app.authorView = new app.AuthorView();
     }
 });
 
