17d72936b21200e26e55cb99782c4d81dd724f37
Hank Wang <drapho@gmail.com>
2013-09-10 22:25:37 +0800

    Added grunt-contrib-ugify

--- camp1-booklog/stage3/Gruntfile.js
+++ camp1-booklog/stage3/Gruntfile.js
@@ -21,6 +21,15 @@ module.exports = function(grunt) {
                 }
             }
         },
+        uglify: {
+            dist: {
+                files: {
+                    'public/js/main.min.js': [
+                            'public/js/main.js'
+                        ]
+                }
+            }
+        },
         exec: {
             start_server: {
                 cmd: 'NODE_ENV=production node app.js'
@@ -34,9 +43,11 @@ module.exports = function(grunt) {
     // load plugins
     grunt.loadNpmTasks('grunt-exec');
     grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     // default task(s)
     grunt.registerTask('start', ['exec:start_server']);
     grunt.registerTask('start-dev', ['exec:start_dev_server']);
+    grunt.registerTask('build', ['sass:dist', 'uglify']);
     grunt.registerTask('default', ['start']);
 };
