e91df9e561f96bab74bc9c5e484f92ffaf18fc9b
Hank Wang <drapho@gmail.com>
2013-09-10 16:57:01 +0800

    Added stage1-with-final-structure

---
+++ camp1-booklog/stage1-with-final-structure/init.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+mkdir -p public/js
+mkdir -p public/css
+mkdir -p public/images
+mkdir -p public/vendor
+mkdir -p public/fonts
+mkdir -p views
+mkdir -p sass
+mkdir -p schema
+mkdir -p scripts
+mkdir -p utilities
+mkdir -p fixes
+
+touch public/.gitignore
+touch views/.gitignore
+touch sass/.gitignore
+touch schema/.gitignore
+touch scripts/.gitignore
+touch utilities/.gitignore
+touch fixes/.gitignore
+
+touch LICENSE
+touch Gruntfile.js
+touch package.json
+touch bower.json
+touch .bowerrc
+touch .gitignore
+touch .editorconfig
